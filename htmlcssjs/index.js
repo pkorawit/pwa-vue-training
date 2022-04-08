e50676743d76a6e02497b7e10ff951d67108ca70
Korawit Prutsachainimmit <korawit.p@gmail.com>
2022-04-08 18:47:14 +0700

    add Html Css Js review

---
+++ htmlcssjs/index.js
@@ -0,0 +1,28 @@
+function register(){
+    const email = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    const sid = document.getElementById("sid").value
+    
+    if(email == ""){
+        alert("Please fill email");
+        document.getElementById("email").focus();
+        return;
+    }
+
+    if(name == ""){
+        alert("Please fill name");
+        document.getElementById("name").focus();
+        return;
+    }
+
+    if(sid == ""){
+        alert("Please fill student ID");
+        document.getElementById("sid").focus();
+        return;
+    }
+
+    console.log(email);
+    console.log(name);
+    console.log(sid);
+    alert("Registration completed!")
+}
